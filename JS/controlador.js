ac0148ae5c4dd20626e2ddc2ecda363585a5f3f7
EdgarOrtiz10 <2120eaoe@gmail.com>
2022-03-26 13:06:48 -0500

    subiendo carrito

--- JS/controlador.js
+++ JS/controlador.js
@@ -27,5 +27,33 @@ contenedorTienda.addEventListener("click",function(event){
         //console.log("añadicion al car")
         //1. capturar la cantidad of product
         let cantidadproducto=document.getElementById("cantidadProducto").value
-        console.log(cantidadproducto)
+        
+        //2. agregar la cantidad al objeto en este caso producto
+        producto.cantidad=cantidadproducto
+        
+        // 3. agregar lo capturado en el paso 2 al carrito
+        //palabra reserbada PUSH para agregar un elemento a un arreglo
+        //push agrega un elemento a un arreglo
+        carrito.push(producto)
+        
+
+        //tarea calcular la sumatoria de cantidades
+        //recorrer el arreglo y sacar la cantidad se rrecorre con un foreach
+        let suma=0
+        carrito.forEach(function (producto) {
+        
+            //console.log(producto.cantidad)    
+            suma=suma+Number(producto.cantidad)
+        })
+        let capsula=document.getElementById("capsula")
+        capsula.textContent=suma
+        capsula.classList.remove("invisible")
+        //el .value solo es para los imputs
+    })
+
+    let limpiar =document.getElementById("btnLimpiar")
+    limpiar.addEventListener('click', function(event){
+
+        carrito=[]
+        capsula.classList.add("invisible")
     })
